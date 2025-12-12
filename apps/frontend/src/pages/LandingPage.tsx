3f32a20fed3e41d8a2edb4dd12966cce80b1a1cd
roy <roy@xafinsight.com>
2025-12-12 23:42:10 +0100

    feat: automatically navigate to the dashboard once the user is logged in

--- apps/frontend/src/pages/LandingPage.tsx
+++ apps/frontend/src/pages/LandingPage.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+
 import IntroductionSection from "../components/IntroductionSection";
 import FeaturesSection from "../components/FeaturesSection";
 import CallToActionSection from "../components/CallToActionSection";
+import { useAuth } from "../hooks/User";
+import { useMemo } from "react";
 
 const LandingPage: React.FC = () => {
+const user = useAuth();
+
+useMemo(() => {
+    if (user.user) {
+      window.location.href = "/dashboard";
+    }
+  }, [user]);
+
   return (
     <div className="bg-gray-50 dark:bg-gray-900">
       <IntroductionSection />
