e903661cbc1e302dd5cdc11bba59decaa2560b44
Roy <github@kolhorn.info>
2025-12-15 21:57:29 +0100

    fix migration paths

--- apps/backend/prisma/auth/prisma.config.ts
+++ apps/backend/prisma/auth/prisma.config.ts
@@ -2,10 +2,9 @@ import 'dotenv/config'
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: 'schema.prisma',
   migrations: { 
-    path: 'prisma/migrations',
-    seed: 'tsx prisma/seed.ts',
+    path: 'migrations',
   },
   datasource: { 
     url: env("AUTH_DATABASE_URL") 
